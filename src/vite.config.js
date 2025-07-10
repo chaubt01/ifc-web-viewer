694704be3dfb07e09e6ea245b1ddb786508138bf
chaubt01 <nvnchau.bt@gmail.com>
2025-07-10 11:44:04 +0700

    Fix proxy download-ifc handl21er

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,13 +1,6 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
+export default {
+  root: ".",
   server: {
-    proxy: {
-      '/nextcloud': {
-        target: 'http://192.168.1.9:30027', // ← địa chỉ IP máy chạy Nextcloud
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/nextcloud/, '/s/EFyP8JafKFSQE26'), // ← mã chia sẻ
-      },
-    },
-  },
-});
+    port: 5173
+  }
+};
